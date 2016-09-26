5fd7d077e34ecf696fdc61de3ca9bb8ed06172ee
Pedro Bernardes <phec06@gmail.com>
2016-09-26 15:29:14 -0300

    Installed devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :authenticate_user!
   protect_from_forgery with: :exception
 end
